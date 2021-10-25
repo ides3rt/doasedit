feaac46cd469c1be5d9ff60a0e66bfc19f085699
ides3rt <des3rtgames@protonmail.com>
2021-10-25 16:04:52 +0000

    Improve everything lol

--- installer.sh
+++ installer.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-[ ! -f '/usr/bin/doas' ] && echo 'doasedit: Package `doas` required by `doasedit`...' && exit 1
+ERR(){
+	printf '%s\n' "doasedit: $2" && exit "$1"
+}
 
-[ ! -f 'doasedit/doasedit' ] && echo 'doasedit: Please be in the same directory as `doasedit`...' && exit 1
+[ -z "$(type -t doas)" ] && ERR 1 'package `doas` required by `doasedit`...'
+
+FullPath="$(realpath $0)"; File="${FullPath%/*}/doasedit"
+
+[ ! -f "$File" -o ! -r "$File" ] && ERR 1 "can't access $File"
 
 [ ! -d "$HOME/.local/bin" ] && mkdir -p $HOME/.local/bin
 
-cp doasedit/doasedit $HOME/.local/bin/
+cp $File $HOME/.local/bin/
 
 chmod u+x $HOME/.local/bin/doasedit
 
-echo 'doasedit: Done...'
+unset ERR FullPath File
