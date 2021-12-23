53bf955458ff9faa7a742f936b42f728010651fd
ides3rt <ides3rt@tutanota.com>
2021-12-23 11:08:34 +0000

    clean

--- installer.sh
+++ installer.sh
@@ -2,29 +2,27 @@
 
 trap 'sync' EXIT
 
-ERR() {
-	printf '%s\n' "ERR: $2" 1>&2
-	(($1 > 0)) && exit $1
+Err() {
+	printf '%s\n' "Err: $2" 1>&2
+	(( $1 > 0 )) && exit $1
 }
 
 type -P doas &>/dev/null ||
-	ERR 1 'package doas(1) required by doasedit()...'
+	Err 1 'package doas(1) required by doasedit()...'
 
-File=$(realpath $0)
+File=$(realpath $0) File="${File%/*}/src/doasedit"
 
-File="${File%/*}/src/doasedit"
-
-[[ -f "$File" || -r "$File" ]] || ERR 1 "can't access $File"
+[[ -f "$File" || -r "$File" ]] || Err 1 "can't access '$File'"
 
 if ((UID)); then
 	Dir="$HOME/.local/bin"
 	[[ -d "$Dir" ]] || mkdir -p $Dir
-	cp $File $Dir/
+	cp "$File" $Dir/
 	chmod 700 $Dir/doasedit
 else
 	Dir=/usr/local/bin
-	cp $File $Dir/
+	cp "$File" $Dir/
 	chmod 755 $Dir/doasedit
 if
 
-printf '%s\n' "Installed. Now add \`$Dir\` to your PATH..."
+printf '%s\n' "Installed. Now add '$Dir' to \$PATH..."
