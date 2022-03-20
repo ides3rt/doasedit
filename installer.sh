69698081db4399b5432a5144ca23e412907594b0
ides3rt <ides3rt@tutanota.com>
2022-03-20 20:04:50 +0000

    Clean

--- installer.sh
+++ installer.sh
@@ -2,33 +2,35 @@
 
 trap 'sync' EXIT
 
-Program="${0##*/}"
+progrm=${0##*/}
 
-Err() {
-	printf '%s\n' "$Program: $2" 1>&2
+panic() {
+	printf '%s\n' "$progrm: $2" 1>&2
 	(( $1 > 0 )) && exit $1
 }
 
-(( $# > 0 )) && Err 2 "needn't argument...'"
+(( $# > 0 )) && panic 1 "needn't argument...'"
 
 if ! type -P doas &>/dev/null; then
-	Err 1 'dependency, `doas`, not met...'
+	panic 1 'dependency, `doas`, not met...'
 fi
 
-File="${0%/*}"/src/doasedit
+file=${0%/*}/src/doasedit
 
-[[ -f $File || -r $File ]] || Err 1 "can't access '$File'..."
+[[ -f $file || -r $file ]] || panic 1 "$file: not found..."
 
 if ((UID)); then
-	Dir="$HOME"/.local/bin
-	Perms=700
+	dir=$HOME/.local/bin
+	perm=700
 else
-	Dir=/usr/local/bin
-	Perms=755
+	dir=/usr/local/bin
+	perm=755
 if
 
-[[ -d $Dir ]] || mkdir -p "$Dir"
-cp -- "$File" "$Dir"
-chmod "$Perms" "$Dir"/doasedit
+if [[ ! -d $dir ]]; then
+	mkdir -p "$dir" || panic 1 "$dir: failed to create..."
+fi
 
-printf '%s\n' "Installed. Now, add '$Dir' to \$PATH..."
+cp -- "$file" "$dir" || panic 1 'failed to copy to destination...'
+
+chmod -- "$perm" "$dir"/doasedit || panic 1 'falied to change permissions...'
