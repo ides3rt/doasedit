5a48dae3c645666597079c033c0635e8db5d21f1
iDes3rt <ides3rt@tutanota.com>
2022-04-04 15:40:13 +0000

    Clean it up, I guess.

--- installer.sh
+++ installer.sh
@@ -2,35 +2,35 @@
 
 trap 'sync' EXIT
 
-progrm=${0##*/}
+readonly progrm=${0##*/}
+
+eprintf() { printf "$@" 1>&2; }
 
 panic() {
-	printf '%s\n' "$progrm: $2" 1>&2
-	(( $1 > 0 )) && exit $1
+	eprintf '%s\n' "$progrm: $2"
+	(( $1 )) && exit $1
 }
 
-(( $# > 0 )) && panic 1 "needn't argument...'"
+(( $# )) && panic 1 "needn't argument...'"
 
 if ! type -P doas &>/dev/null; then
 	panic 1 'dependency, `doas`, not met...'
 fi
 
-file=${0%/*}/src/doasedit
+readonly file=${0%/*}/src/doasedit
 
-[[ -f $file || -r $file ]] || panic 1 "$file: not found..."
+[[ -f $file && -r $file ]] || panic 1 "$file: not found..."
 
 if ((UID)); then
-	dir=$HOME/.local/bin
-	perm=700
+	readonly dir=$HOME/.local/bin perm=700
 else
-	dir=/usr/local/bin
-	perm=755
+	readonly dir=/usr/local/bin perm=755
 if
 
 if [[ ! -d $dir ]]; then
-	mkdir -p "$dir" || panic 1 "$dir: failed to create..."
+	mkdir -p -- "$dir" || panic 1 "$dir: failed to create..."
 fi
 
-cp -- "$file" "$dir" || panic 1 'failed to copy to destination...'
+cp -a -- "$file" "$dir" || panic 1 'failed to copy to destination...'
 
-chmod -- "$perm" "$dir"/doasedit || panic 1 'falied to change permissions...'
+chmod -- $perm "$dir"/doasedit || panic 1 'falied to change permissions...'
